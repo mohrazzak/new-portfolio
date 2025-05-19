02345370a20c7083fb12ad7279f84ee17f7eece9
Mohammad Abdalrazzak <mohammad.abdalrazzak@unifisolutions.io>
2025-05-20 00:28:08 +0300

    vite

---
+++ public/assets/js/hoverChangeDescription.js
@@ -0,0 +1,12 @@
+export function hoverChangeDescription(nameCard, text) {
+  var changeDescription = document.querySelector(".changeDescription");
+
+  document.querySelector(nameCard).addEventListener("mouseover", () => {
+    changeDescription.innerHTML = text;
+  });
+
+  document.querySelector(nameCard).addEventListener("mouseout", () => {
+    changeDescription.innerHTML = `*hover over the card to read*`;
+  });
+}
+
